0a7486e1b6bf6d5854218f6026b9be2a25c892c2
Habib Hammam Kurniawan <78804507+habibhkrnwn@users.noreply.github.com>
2022-07-06 20:39:20 +0700

    Add files via upload

---
+++ a jadii.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#define N 255
+
+void replace (char*, char, char);
+main()
+{
+    char data[N], cari, ganti;
+    printf("---Mengganti huruf A menjadi I---\n\n ");
+    printf("Masukan sebuah string : ");
+    scanf("%[^\n]", data);
+    printf("Huruf yang akan diganti : ");
+    getchar ();scanf("%c", &cari);
+    printf("Huruf yang pengganti : ");
+    getchar ();scanf("%c", &ganti);
+    replace (data, cari, ganti);
+    printf("\nHasil pengganti : %s\n\n", data);
+    return 0;
+}
+void replace (char *data, char cari, char replace){
+    int a=0;
+    while(*data !='\0'){
+        *data=((*data == cari)? replace:*data);
+        data++;
+        a++;
+    }
+}
+
